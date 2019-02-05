f0835d9f21a258c2b18b1f12062dbd95c863d51f
Daniyar Marklenovich <daniyar01.m@gmail.com>
2019-02-05 18:48:03 +0600

    master

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 10;
+        x = 5 + 14;
+        System.out.println(x);
+        System.out.println("Привет, мир!");
+        final int Привет = 12;
+        System.out.println(Привет);
+    }
+}
